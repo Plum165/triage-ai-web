ff3da01f91c185e47310a22a34e7f281a7f94498
Rashaad <smsmoe006@myuct.ac.za>
2025-07-26 22:13:20 +0200

    modified:   package.json 	new file:   public/login.html 	new file:   public/signup.html 	modified:   server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const fetch = require("node-fetch"); 
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const app = express();
 const PORT = 3000;
